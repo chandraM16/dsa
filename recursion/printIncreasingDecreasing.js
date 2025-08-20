b5e1061a4979c6037e9cc81b5c05f30b602bb84a
Chandan Marghade <chandanmarghade@gmail.com>
2025-08-21 01:18:39 +0530

    added > printIncreasingDecreasing

---
+++ recursion/printIncreasingDecreasing.js
@@ -0,0 +1,11 @@
+const printIncreasingDecreasing = (num) => {
+    if(num === 0){
+        return;
+    }
+    console.log(num)
+    printIncreasingDecreasing(num -1)
+    console.log(num)
+}
+
+
+printIncreasingDecreasing(5)
