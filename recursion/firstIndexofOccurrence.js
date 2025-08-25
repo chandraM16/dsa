2fbe194adc07de4c004bd093be911e66782cada9
Chandan Marghade <chandanmarghade@gmail.com>
2025-08-25 15:06:58 +0530

    added > firstIndexofOccurrence

--- recursion/firstIndexofOccurrence.js
+++ recursion/firstIndexofOccurrence.js
@@ -1,5 +1,6 @@
 const firstIndexofOccurrence = (arr, num) => {
-  return helperFunction(arr, arr.length - 1, num);
+  //   return helperFunction(arr, arr.length - 1, num);
+  return helperFunction2(arr, 0, num);
 };
 const helperFunction = (arr, idx, num) => {
   if (idx < 0) {
@@ -14,7 +15,18 @@ const helperFunction = (arr, idx, num) => {
   }
   return -1;
 };
+const helperFunction2 = (arr, idx, num) => {
+  if (idx === arr.length) {
+    return -1;
+  }
+  if (arr[idx] === num) {
+    return idx;
+  } else {
+    const foundIdx = helperFunction2(arr, idx + 1, num);
+    return foundIdx;
+  }
+};
 
 console.log(
-  firstIndexofOccurrence([2, 3, 9, 8, 7, 6, 4, 8, 12, 7, 3, 8, 12], 8)
+  firstIndexofOccurrence([2, 3, 9, 8, 7, 6, 4, 8, 12, 7, 3, 8, 12], 2)
 );
