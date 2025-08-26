99f2ea393e8bba0ea66a83f8c6cce5c2f884d929
Chandan Marghade <chandanmarghade@gmail.com>
2025-08-27 00:36:46 +0530

    added > OptimizedQueue

--- dataStructure/queue.js
+++ dataStructure/queue.js
@@ -33,7 +33,42 @@ export default class Queue {
   }
 }
 
-const q = new Queue();
+export class OptimizedQueue {
+  constructor() {
+    this.items = {};
+    this.rear = 0;
+    this.front = 0;
+  }
+
+  enqueue(element) {
+    this.items[this.rear] = element;
+    this.rear++;
+  }
+
+  dequeue() {
+    const item = this.items[this.front];
+    delete this.items[this.front];
+    this.front++;
+    return item;
+  }
+  isEmpty() {
+    return this.rear - this.front === 0;
+  }
+
+  size() {
+    return this.rear - this.front;
+  }
+
+  peek() {
+    return this.items[this.front];
+  }
+
+  print() {
+    console.log(this.items);
+  }
+}
+
+const q = new OptimizedQueue();
 q.enqueue(20);
 q.enqueue(30);
 q.print();
@@ -42,5 +77,3 @@ console.log(q.size());
 
 q.dequeue();
 q.print();
-
-
