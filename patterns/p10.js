ed905caa39d087a610540d9c74f5da02ab585611
Chandan Marghade <chandanmarghade@gmail.com>
2025-09-05 01:07:20 +0530

    added > printPatternP10

---
+++ patterns/p10.js
@@ -0,0 +1,18 @@
+const printPatternP10 = (n) => {
+  for (let i = 1; i <= n; i++) {
+    let str = "";
+    for (let j = 1; j <= i; j++) {
+      str += "* ";
+    }
+    console.log(str);
+  }
+  for (let i = n - 1; i > 0; i--) {
+    let str = "";
+    for (let j = 1; j <= i; j++) {
+      str += "* ";
+    }
+    console.log(str);
+  }
+};
+
+printPatternP10(9);
